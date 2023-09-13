e94e3c501eb0038062008b7d36df0c5b5a3022b5
gbaer <gbaer@EYI-PC>
2023-09-12 20:06:03 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import logo from "./logo.svg";
+import "./App.css";
+import Board from "./Board";
+
+/** Simple app that just shows the Lights Out game */
+
+function App() {
+  return (
+    <div className="App">
+      <Board />
+    </div>
+  );
+}
+
+export default App;
